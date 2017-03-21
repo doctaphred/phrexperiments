85b8dee8c99617b9939317702b7bebb032654a14
Frederick Wagner <doctaphred@gmail.com>
2017-03-20 21:13:21 -0400

    Add redefine and instrument

--- patch.py
+++ patch.py
@@ -20,3 +20,25 @@ def patch(obj, *delattrs, **setattrs):
                 delattr(obj, name)
             else:
                 setattr(obj, name, value)
+
+
+@contextmanager
+def redefine(func, new_func):
+    """Redefine func to behave like new_func.
+
+    All references to func -- even those in other scopes or modules --
+    will exhibit the new behavior until this context manager exits.
+    """
+    original_code = func.__code__
+    func.__code__ = new_func.__code__
+    try:
+        yield
+    finally:
+        func.__code__ = original_code
+    # alternatively: patch(func, __code__=new_func.__code__)
+
+
+def instrument(func, decorator):
+    """Remotely decorate func with decorator within this context manager."""
+    return redefine(func, decorator(func))
+    # alternatively: patch(func, __code__=decorator(func).__code__)
