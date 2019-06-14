48caa748651170047e4182b530d1458ef38d8a75
Frederick Wagner <doctaphred@gmail.com>
2019-06-14 14:07:39 -0500

    Update autoimport.py

--- autoimport.py
+++ autoimport.py
@@ -1,67 +1,63 @@
+"""Don't try this at home."""
 import time
 from subprocess import run
 from importlib import import_module
 
 
-def autoimport(name, attempt, expect=Exception):
+def autoimport(name, otherwise=Exception):
+    print(f"Couldn't find {name}. You probably just forgot to import it!")
+    time.sleep(2)
     try:
-        obj = attempt(name)
-    except expect as exc:
-        if name.startswith('_'):
-            raise
-        print(f"Checking globals for {name}...")
-        time.sleep(0.5)
+        obj = import_module(name)
+    except ModuleNotFoundError:
+        print(f"Hm, don't see nay modules named '{name}'.")
+        print(f"You must have forgotten to install it! Let's fix that...")
+        time.sleep(4)
         try:
-            obj = globals()[name]
-        except KeyError:
-            print(f"Didn't find it there.")
-            print(f"Maybe it's a builtin?")
-            time.sleep(1)
-            try:
-                obj = getattr(__builtins__, name)
-            except AttributeError:
-                print(f"Nope, not there either. Hmm.")
-                print(f"Ah, you probably just forgot to `import {name}`.")
-                time.sleep(2)
-                try:
-                    obj = import_module(name)
-                except ModuleNotFoundError:
-                    print(f"Hm, don't see nay modules named '{name}'.")
-                    print(
-                        f"You must have forgotten to install it!"
-                        f" Let's fix that..."
-                    )
-                    time.sleep(4)
-                    try:
-                        run(['pip', 'install', name], check=True)
-                        obj = import_module(name)
-                    except Exception:
-                        print(f"Aw, that didn't work :( Sorry!")
-                        raise exc
-                    else:
-                        print(f"There you go, now it's installed! :D")
+            run(['pip', 'install', name], check=True)
+            obj = import_module(name)
+        except Exception:
+            print(f"Aw, that didn't work :( Sorry!")
+            raise otherwise
 
-    print(f"Yep! Here you go: {obj}")
+    print(f"Found it! Here you go: {obj}")
     return obj
 
 
-class ImportingDict(dict):
-    def __getitem__(self, key):
-        return autoimport(key, super().__getitem__, KeyError)
+class AutoImportDict(dict):
+    def __missing__(self, key):
+        if not isinstance(key, str) or key.startswith('_'):
+            raise KeyError(key)
+
+        try:
+            return globals()[key]
+        except KeyError:
+            pass
+
+        try:
+            return getattr(__builtins__, key)
+        except AttributeError:
+            pass
+
+        return autoimport(key, KeyError(key))
 
 
-class DontTryThisAtHome(type):
+class AutoImportMeta(type):
     def __prepare__(meta, *args, **kwargs):
-        return ImportingDict()
+        return AutoImportDict()
 
-    def __getattribute__(self, name):
-        return autoimport(name, super().__getattribute__, AttributeError)
+    def __getattr__(self, name):
+        return autoimport(name, AttributeError(name))
 
 
 if __name__ == '__main__':
     url = 'https://raw.githubusercontent.com/doctaphred/emojencode/master/LICENSE'  # noqa
 
-    class packages(metaclass=DontTryThisAtHome):
+    class packages(metaclass=AutoImportMeta):
+
+        if sys.argv[1:2] != ['--i-understand-the-consequences-of-my-actions']:  # noqa
+            exit("You don't understand the consequences of your actions.")
+
         print(requests.get(url).text)  # noqa: F821 undefined name
 
     expected = '😘😗😥😹😈😆😱😭😘😖😼💩'
